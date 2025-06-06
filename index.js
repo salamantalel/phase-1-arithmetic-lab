513e81f7acc134fcf4802372a06a6303596d0a8f
salamantalel <nmoinkett@gmail.com>
2025-06-06 13:36:32 +0300

    add arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+ // Multiply
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; // should equal 62
 
-// Write your code here
+// Random number greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Modulo to get remainder 4
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4; // should equal 4
+
+// Find max number
+const max = Math.max(1, 3, 5, 10, 20);
